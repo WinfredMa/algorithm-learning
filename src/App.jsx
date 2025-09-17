c020a0ebac5f0b7b88ac4ef45f37c9b676f06b43
winfred <851936509@qq.com>
2025-09-17 15:34:50 +0800

    algorithm:refine recover sorted string.

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div class="algorithms-home">
+    <div className="algorithms-home">
       <RecoveryString />
     </div>
   )
--- src/algorithms/recoveryString.jsx
+++ src/algorithms/recoveryString.jsx
@@ -1,49 +1,74 @@
 function RecoveryString() {
-  const numberCount = 4;
-  const string = '432111111111';
-  const strObjA = generateStrObj(string);
+  const testData = [
+    {
+      numberCount: 5,
+      string: '19801211',
+      expected: '8',
+    },
+    {
+      numberCount: 4,
+      string: '432111111111',
+      expected: '111',
+    },
+  ];
+  const numberCount = 5;
+  const string = '19801211';
   let result;
-
-  for (let i = 0; i <= 1000 - numberCount + 1; i++) {
-    let tempStr = '';
-    for (let j = i; j < i + numberCount; j++) {
-      tempStr += j.toString();
+  const algorithm = (numberCount, string) => {
+    // the definition of Map and its methods should be enhanced
+    const sortedStrMap = new Map();
+    for (let char of string) {
+      // the difference between for in and for of
+      sortedStrMap.set(char, (sortedStrMap.get(char) || 0) + 1);
     }
-    let strObjB = generateStrObj(tempStr);
-    let isMatch = true;
+    for (let i = 0; i <= 1000 - numberCount + 1; i++) {
+      let tempStr = '';
+      for (let j = i; j < i + numberCount; j++) {
+        tempStr += j.toString();
+      }
 
-    if (Object.keys(strObjA).length === Object.keys(strObjB).length) {
-      for (let key in strObjA) {
-        if (strObjA[key] !== strObjB[key]) {
+      const origionalStrMap = new Map();
+      for (let char of tempStr) {
+        origionalStrMap.set(char, (origionalStrMap.get(char) || 0) + 1);
+      }
+      let isMatch = true;
+      // Map.keys and sortedStrMap are different.
+      for (let key of sortedStrMap.keys()) {
+        if (
+          sortedStrMap.size !== origionalStrMap.size ||
+          sortedStrMap.get(key) !== origionalStrMap.get(key)
+        ) {
           isMatch = false;
           break;
         }
       }
-    } else {
-      isMatch = false;
+
+      if (isMatch) {
+        result = i;
+        break;
+      }
     }
 
-    if (isMatch) {
-      result = i;
-      break;
-    }
+    return result;
+  };
+  for (let i = 0; i < testData.length; i++) {
+    const { numberCount, string } = testData[i];
+    testData[i].result = algorithm(numberCount, string);
   }
-
-  return <div>{result}</div>;
-}
-
-function generateStrObj(string) {
-  const strObj = {};
-
-  for (let i = 0; i < string.length; i++) {
-    if (!strObj[string[i]]) {
-      strObj[string[i]] = 1;
-    } else {
-      strObj[string[i]] += 1;
-    }
-  }
-
-  return strObj;
+  return (
+    <div>
+      {testData.map((testDataItem) => {
+        return (
+          <div key={testDataItem.string}>
+            <div>numberCount: {testDataItem.numberCount}</div>
+            <div>string: {testDataItem.string}</div>
+            <div>expected: {testDataItem.expected}</div>
+            <div>result: {testDataItem.result}</div>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
 
 export default RecoveryString;
